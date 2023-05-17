8305cb180b2a3d77e2a8ad90b48106c1be9ced11
Isaac Newton <mugoisaacnewton@gmail.com>
2023-05-17 12:18:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cryptoverse - Explore the World of Cryptocurrency
+
+This is a code repository for the creation a cryptocurrency app using React and multiple APIs powered by https://rapidapi.com.
+
+
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': '09ef6a1363msh5729b2d57369354p197a37jsna076e6537603',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://bing-news-search1.p.rapidapi.com' }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
