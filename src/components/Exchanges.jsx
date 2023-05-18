ae6a84e4022be24409aebd5b9c4a8710c1a3cd1e
Isaac Newton <mugoisaacnewton@gmail.com>
2023-05-18 16:03:52 +0300

    third commit

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
 import millify from 'millify';
-// eslint-disable-next-line no-unused-vars
 import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-// eslint-disable-next-line no-unused-vars
 import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
-// eslint-disable-next-line no-unused-vars
 const { Text } = Typography;
-// eslint-disable-next-line no-unused-vars
 const { Panel } = Collapse;
 
 const Exchanges = () => {
